1099fb1c0538853ba2fd8fe1c8cde115a4df5764
Ingrid <ingrid.meccas@gmail.com>
2022-01-04 16:41:41 +0000

    Update in Index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Required modules for this app to work, copied from class*/
+/* Required modules for this app to work, copied from class */
 
 const   http = require('http'), //This module provides the HTTP server functionalities
         path = require('path'), //The path module provides utilities for working with file and directory paths
@@ -7,3 +7,31 @@ const   http = require('http'), //This module provides the HTTP server functiona
         xmlParse = require('xslt-processor').xmlParse, //This module allows to work with XML files
         xsltProcess = require('xslt-processor').xsltProcess, //The same module allows us to uitlise XSL Transformations
         xml2js = require('xml2js'); //This module does XML <-> JSON conversion
+
+const router = express(); //Creates new instance 
+const server = http.createServer(router); //Creates server instance
+
+router.get('/', function(req, res) {
+
+    res.writeHead(200, {'Content-Type' : 'text/html'}); 
+    
+    /* Reads files */
+    let xml = fs.readFileSync('t-shop.xml', 'utf8'), 
+        xsl = fs.readFileSync('t-shop.xsl', 'utf8');
+
+    /* Parses so the string becomes xml objects */
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    /* Applies the xsltprocess to get a result */
+    let result = xsltProcess(doc, stylesheet);
+
+    /* Converts the html file back to String for user */
+    res.end(result.toString());
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
